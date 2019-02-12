d0ef7a44276f747431ae68ec008106d77bdea99a
warlenpinheiro <warlen@live.com>
2019-02-12 18:38:22 -0200

    Arquivo solução do desafio
    Arquivo busca.php com solução do desafio e arquivo com os dados no formato csv.

---
+++ busca.php
@@ -0,0 +1,30 @@
+<?php
+
+	$filename = "employers_list.csv";
+	try {
+		if(file_exists($filename) && is_readable($filename)){
+			$fh = fopen($filename, "r");
+			while (($line = fgetcsv($fh)) !== false){
+			  $meuArray[] = $line;
+			}
+			fwrite(STDOUT, "Digite um nome ou cargo:");
+			$name = fgets(STDIN);
+			$name = trim(ucwords($name));
+			$chave = "/(?i){$name}/";
+
+			foreach ($meuArray as $collection) {
+				if (preg_match($chave, $collection[0]." ".$collection[1]) or preg_match($chave, $collection[3])) {
+					$result = array_combine($meuArray[0], $collection);
+					print_r($result);
+					echo "\n";
+				}
+			}
+			fclose($fh);
+			exit(0);
+		}else{
+			throw new Exception("Nao foi possivel ler os dados", 1);
+		}
+	} catch (Exception $e) {
+		fwrite(STDOUT, $e->getMessage());
+		exit(1);
+	}
